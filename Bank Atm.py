bbfd7515867f891e79ec2b306b54766acf6a0b8c
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2022-07-07 21:55:04 +0530

    Add files via upload

---
+++ Bank Atm.py
@@ -0,0 +1,13 @@
+class Atm():
+    def __init__(self,cardNo,pinNo):
+        self.cardNo = cardNo
+        self.pinNo = pinNo
+
+    def cashWithDrowl(self):
+        print("Cash WithDrowl")
+
+    def balanceEnquiry(self):
+        print("Balance Enquiry")
+
+# user1 = Atm(100,200)
+# user2 = Atm(300,400)
