34ae665626d631b46386ad0a85243145d4f2e730
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2022-08-03 21:06:21 +0530

    Add files via upload

--- Bank Atm.py
+++ Bank Atm.py
@@ -2,17 +2,27 @@ class Atm:
     def __init__(self,cardNo,pinNo):
         self.cardNo = cardNo
         self.pinNo = pinNo
+    
+    language = input("Select your Language: ")
+    pinNo = int(input("Enter 4-digit Pin Number: "))
+    cardNo = int(input("Enter your Card Number: "))
+    balance = 25000
+    def balanceEnquiry():
+        balance = 25000
+        print("Your Balance:- ",balance)
+    
+    def moneyWithdraw():
+        amount = int(input("Enter your Amount: "))
+        print("Successfuly Money Withdraw!")
 
-    def cashWithDrowl(self):
-        print("Cash WithDrowl")
-        print(self.cardNo,self.pinNo)
+    print("Choose Action by enter a or b")
+    print("a. Money Withdraw")
+    print("b. Balance Enquiry")
+    action = input()
 
-    def balanceEnquiry(self):
-        print("Balance Enquiry")
-        print(self.cardNo,self.pinNo)
-
-user1 = Atm(100,200)
-user2 = Atm(300,400)
-
-user1.cashWithDrowl()
-user2.balanceEnquiry()
+    if(action == "a"):
+        moneyWithdraw()
+    elif(action == "b"):
+        balanceEnquiry()
+    else:
+        print("Error!")
